f51ee9165214d8570a7a58f4b12b4754086080ca
Fredrik-M <47598219+Fredrik-M@users.noreply.github.com>
2019-12-02 12:57:06 +0100

    Project part 3

--- Projekt/trapet.m
+++ Projekt/trapet.m
@@ -1,10 +1,11 @@
-function result = trapet(f, start, End)
 % It applies the trapets method on a vector of values of f function.
 % start is the initial value, End is the last value.
-result = 0;
+
+function result = trapet(f, start, End)
+%result = 0;
 step = (End - start)/(length(f) -1);
-for i = 1:length(f)-1
-    result = result + step*(f(i) + f(i+1))/2;
-end
-end
+
+v = (0.5 * step) .* ones(1, length(f) - 1);
+g = (f(1:length(f) - 1) + f(2:length(f)))';
+result = v * g;
 
