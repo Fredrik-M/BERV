cb59520bccc482619ddb594fbd404e507906f86f
jake991 <jacoub96_2011@hotmail.com>
2019-12-02 12:36:16 +0100

    fixed comments

--- Projekt/range.m
+++ Projekt/range.m
@@ -1,6 +1,7 @@
+% Return how many kilometers can the car drive more, given its
+% model, the route it drives at and the charge it started with using
+% simpson method to approaximate the range.
 function r = range(x, carmodel, route, charge, n)
-% Return how may kilometers can the car drive more, given its model, the
-% route, the charge it started at, and the distance it has already passed.
 total_consumption = tot_consumption(x, carmodel, route, n);
 average_consumption = total_consumption/x;
 remaining_charge = charge - total_consumption;
--- Projekt/trapet.m
+++ Projekt/trapet.m
@@ -1,6 +1,7 @@
-function result = trapet(f, start, End)
+
 % It applies the trapets method on a vector of values of f function.
 % start is the initial value, End is the last value.
+function result = trapet(f, start, End)
 result = 0;
 step = (End - start)/(length(f) -1);
 for i = 1:length(f)-1
