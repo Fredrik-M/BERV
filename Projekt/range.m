54882e66306b0eeb9d3c6f5cf218336b01e4c55c
Fredrik Mattisson <47598219+Fredrik-M@users.noreply.github.com>
2019-12-13 10:39:06 +0100

    Update range.m

--- Projekt/range.m
+++ Projekt/range.m
@@ -1,9 +1,9 @@
 % Return how many kilometers can the car drive more, given its
 % model, the route it drives at and the charge it started with using
 % simpson method to approaximate the range.
-function r = range(x, carmodel, route, charge, n)
+function r = range(x, model, route, charge, n)
 
-total_consumption = tot_consumption(x, carmodel, route, n);
+total_consumption = tot_consumption(x, model, route, n);
 average_consumption = total_consumption/x;
 remaining_charge = charge - total_consumption;
 
